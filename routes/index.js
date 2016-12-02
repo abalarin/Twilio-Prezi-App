c4acd656e04861f1502115ea2c4365abf054201e
abalarin <abalarin@ramapo.edu>
2016-12-02 16:15:15 -0500

    import and incl websockets

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 var express = require('express');
 var router = express.Router();
+
+//var fs = require('fs');
 
 var accountSid = 'ACea45b8f40c69d717e0b0c31d62b5f3e9';
 var authToken = 'c90bc2bbb89615700d63aa53cc96b03b';
 var client = require('twilio')(accountSid, authToken);
 
-var mssgs = [];
+//var readableStream = fs.createReadStream(client.messages);
 
+var mssgs = [];
 client.messages.list(function(err, data) {
     data.messages.forEach(function(message) {
         if(message.from != '+12015741835'){
-          mssgs.push({from : message.from, body: message.body});
+
+          mssgs.push({from: message.from, body: message.body});
           console.log(message.body);
         }
     });
