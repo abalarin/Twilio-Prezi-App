76df44a5739fdf1cb124886e0b122f7fff5bab62
abalarin <abalarin@ramapo.edu>
2016-12-05 15:04:06 -0500

    pushed screen shots

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,5 @@
 var listSize = 0;
 $(function (){
-  // var jax = $.post( "/", function( data ) {
-  //   listSize = data.data.length;
-  //   for (var i = 0; i < data.data.length; i++) {
-  //     var size = data.data[i].Body.length;
-  //     console.log(size);
-  //     for (var j = 0; j < size; j++) {
-  //       var msg = data.data[i].Body[j];
-  //       $("ul").prepend('<li id=' + msg + '>' + msg + '</li>');
-  //     }
-  //   }
-  // });
-
   setInterval(function(){
     $.post( "/", function( data ) {
       currentListSize = data.data.length;
